6c79b0e1d6eacfb9634fc4d057c50c5f9613f874
ningyanke <ningyanke@outlook.com>
2020-09-17 00:34:59 +0800

    Site updated: 2020-09-17 00:34:58

---
+++ commic/static/js/tongji.js
@@ -0,0 +1,9 @@
+document.writeln("<div style=\"display:none;\">");
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?407f502eb914179baa7e4efa6da65a6e";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+document.writeln("</div>");
