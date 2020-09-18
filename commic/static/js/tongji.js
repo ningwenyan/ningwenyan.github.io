22742b4c3f0a3a3964975c9dfaf2eb534fc1eb23
ningyanke <ningyanke@outlook.com>
2020-09-19 02:11:35 +0800

    Site updated: 2020-09-19 02:11:34

--- commic/static/js/tongji.js
+++ commic/static/js/tongji.js
@@ -7,3 +7,10 @@ var _hmt = _hmt || [];
     s.parentNode.insertBefore(hm, s);
 })();
 document.writeln("</div>");
+(function() {
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://tva1.sinaimg.cn/large/a20ab060ly1givcpo78k2j200h00g0bz.jpg';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}());
