68f29e0e0806343b86502c95aea71d83e46da88d
ningwenyan <ningyanke@outlook.com>
2023-05-29 23:01:34 +0800

    Site updated: 2023-05-29 23:00:41

--- commic/static/js/tongji.js
+++ commic/static/js/tongji.js
@@ -2,7 +2,7 @@ document.writeln("<div style=\"display:none;\">");
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?407f502eb914179baa7e4efa6da65a6e";
+    hm.src = "https://hm.baidu.com/hm.js?81f15ea26022343dff3264644989d1d8";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
