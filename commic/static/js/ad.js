1e3cc7a0fd5fe22ef3d55bc974bedd9f7ff95dc0
ningwenyan <ningyanke@outlook.com>
2023-05-30 21:35:50 +0800

    Site updated: 2023-05-30 21:35:26

--- commic/static/js/ad.js
+++
@@ -1,6 +0,0 @@
-// 这是测试js文件
-var adScript = document.createElement('script');
-adScript.setAttribute('async', '');
-adScript.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4337512190549598');
-adScript.setAttribute('crossorigin', 'anonymous')
-document.head.appendChild(adScript);
